295a0f18816c5065940b88401144624a82e6d534
Christine Frank and Carrie Schmid <carrie@>
2019-10-07 11:40:44 -0700

    Add form and properties for halloween.

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -6,7 +6,31 @@ namespace MadLib.Controllers
   public class HomeController : Controller
   {
       [Route("/")]
+      public ActionResult StoryChooser() { return View(); }
+
+      [Route("/form")]
       public ActionResult Form() { return View(); }
+
+      [Route("/halloween")]
+      public ActionResult Story(string noun, string verb_ing, string noun2, string noun3, string bodypart, string noun4, string verb_ing2, string bodypart_pl, string verb_ing3, string furniture, string noun5, string bodypart2, string adverb, string bodypart3)
+      {
+          MadLibEntries entries = new MadLibEntries();
+          entries.Noun = noun;
+          entries.Noun2 = noun2;
+          entries.Noun3 = noun3;
+          entries.Noun4 = noun4;
+          entries.Verb_ing2 = verb_ing2;
+          entries.Verb_ing = verb_ing;
+          entries.Verb_ing3 = verb_ing3;
+          entries.Bodypart_pl = bodypart_pl;
+          entries.Bodypart2 = bodypart2;
+          entries.Bodypart3 = bodypart3;
+          entries.Bodypart = bodypart;
+          entries.Adverb = adverb;
+          entries.Furniture = furniture;
+
+          return View(entries); 
+      } 
 
       [Route("/story")]
       public ActionResult Story(string noun, string verb, string adjective, string animal, string persontwo, string personone) { 
--- MadLibs/Models/MadLibEntries.cs
+++ MadLibs/Models/MadLibEntries.cs
@@ -8,5 +8,20 @@ namespace MadLib.Models
         public string Animal { get; set; }
         public string PersonTwo { get; set; }
         public string PersonOne { get; set; }
+
+        public string Noun2 { get; set; }
+        public string Noun3 { get; set; }
+        public string Noun4 { get; set; }
+
+        public string Verb_ing { get; set; }
+        public string Verb_ing2 { get; set; }
+        public string Verb_ing3 { get; set; }
+
+        public string Bodypart_pl { get; set; }
+        public string Bodypart2 { get; set; }
+        public string Bodypart3 { get; set; }
+        public string Bodypart { get; set; }
+        public string Adverb { get; set; }
+        public string Furniture { get; set; }
     }
 }
